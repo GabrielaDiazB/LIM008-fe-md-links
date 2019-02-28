7c5d2639ff88158079d58a88f3125e6639608fd1
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-02-27 19:15:40 -0500

    se añadió nuevos archivos, se corrigieron nombres y se añadió arquitectura para los tests

--- src/paths.js
+++ src/paths.js
@@ -1,9 +1,14 @@
 let path = require('path');
-let fileRoute = path.isAbsolute('path.js');
-let routeArr = [];
-if (!fileRoute) {
-  routeArr.push(path.resolve('path.js'));
-} else {
-  routeArr.push(path);
-}
-console.log(routeArr);
+const pathType = (path) => {
+  return pathType.isAbsolute(path);
+};
+
+// let path = require('path');
+// let fileRoute = path.isAbsolute('path.js');
+// let routeArr = [];
+// if (!fileRoute) {
+//   routeArr.push(path.resolve('path.js'));
+// } else {
+//   routeArr.push(path);
+// }
+// console.log(routeArr);
---
+++ test/path-test.spec.js
@@ -0,0 +1,20 @@
+describe('', () => {
+  it('Debería ser una función', () => {
+    expect(typeof ).toBe('function');
+  });
+});
+describe('', () => {
+  it('Debería decir verdadero si la ruta es absoluta', () => {
+      expect().toEqual()
+  });
+});
+describe('', () => {
+  it('Debería decir falso si la ruta es relativa', () => {
+    expect().toEqual()
+  });
+});
+describe('', () => {
+  it('Debería convertir la ruta relativa en absoluta', () => {
+    expect().toEqual()
+  });
+});
