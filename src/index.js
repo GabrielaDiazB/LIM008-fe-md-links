60c55629b54876b4b9d377148cd6fd470f3b7536
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-03-14 14:01:12 -0500

    Se corrigiio un error en la funcion mdlinks

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import {extractLinks} from './functions-controller/arrays.js';
 import {validateLinks} from './functions-controller/options.js';
-const options = {
-  validate: false
-}; 
+
 export const mdLinks = (path, options) => {
-  if (options.validate) {
+  if (options.validate === true) {
     return validateLinks(path)
       .then(response => console.log(response))
       .catch(error => console.log(error));
-  };
-  if (options.validate === false) {
+  } else {
     return new Promise(resolve => resolve(extractLinks(path)));
-  }; 
+  }
 };
 // console.log(mdLinks('test', options));
